11b254fc0f1ae002d014f8dbb4587396a2c01edf
Pedro Henrique Migliatti <pedro.migliatti@gmail.com>
2018-04-18 10:59:56 -0300

    Mapped the rest of the properties

--- src/main/java/eu/wdaqua/dblp/Utility.java
+++ src/main/java/eu/wdaqua/dblp/Utility.java
@@ -20,20 +20,20 @@ public class Utility {
     public static final String URL = " <http://purl.org/dc/elements/1.1/identifier> ";
     public static final String URL_WWW = " <http://schema.org/url> ";
     public static final String EE = " <http://www.w3.org/2002/07/owl#sameAs> ";
-    public static final String NOTE = " <note> ";
-    public static final String CITE = " <cite> ";
+    public static final String NOTE = " <http://www.w3.org/2004/02/skos/core#note> "; //I could not map
+    public static final String CITE = " <http://www.w3.org/1999/xhtml/vocab#cite> "; //I could not map
     public static final String EDITOR = " <http://schema.org/editor> ";
     public static final String BOOKTITLE = " <booktitle> ";
     public static final String ADDRESS = "<http://schema.org/address>";
     public static final String MONTH = " <http://purl.org/dc/terms/issued> ";
-    public static final String CDROM = " <cdrom> ";
+    public static final String CDROM = " <http://lsdis.cs.uga.edu/projects/semdis/opus#cdrom> ";
     public static final String PUBLISHER = " <http://schema.org/publisher> ";
-    public static final String CROSSREF = " <crossref> ";
-    public static final String ISBN = " <isbn> ";
-    public static final String SERIES = " <series> ";
-    public static final String SCHOOL = " <school> ";
-    public static final String CHAPTER = " <chapter> ";
-    public static final String PUBLNR = " <publnr> ";
+    public static final String CROSSREF = " <http://purl.org/net/nknouf/ns/bibtex#hasCrossref> ";
+    public static final String ISBN = " <http://schema.org/isbn> ";
+    public static final String SERIES = " <http://lsdis.cs.uga.edu/projects/semdis/opus#in_series> ";
+    public static final String SCHOOL = " <http://lsdis.cs.uga.edu/projects/semdis/opus#School> ";
+    public static final String CHAPTER = " <http://lsdis.cs.uga.edu/projects/semdis/opus#chapter> ";
+    public static final String PUBLNR = " <publnr> "; //I could not map
 
 
 
