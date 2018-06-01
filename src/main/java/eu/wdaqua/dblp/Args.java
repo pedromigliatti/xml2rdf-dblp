709a974a1cfa354be58450a36cf31fc1f376bf7f
p3m1 <pedro.migliatti@gmail.com>
2018-06-01 15:34:38 +0200

    Remove the default paramters

--- src/main/java/eu/wdaqua/dblp/Args.java
+++ src/main/java/eu/wdaqua/dblp/Args.java
@@ -4,8 +4,8 @@ import com.beust.jcommander.Parameter;
 
 public class Args {
     @Parameter(names={"--input", "-i"})
-    String input = "/home_expes/dd77474h/datasets/dblp_new/dump/dblp.xml" ;
+    String input;
 
     @Parameter(names={"--output", "-o"})
-    String output = "/home_expes/dd77474h/datasets/dblp_new/dump/dump.nt";
+    String output;
 }
